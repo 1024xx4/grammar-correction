6205fef16281d4e2962ab7e475f51fbe1eb7448d
tera <1024xx4@gmail.com>
2024-06-23 18:14:05 +0000

    Feat: Flask で REST API server を実装する

---
+++ backend/flask_example.py
@@ -0,0 +1,14 @@
+import json
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/hello', methods=['POST'])
+def hello_service():
+    json_data = request.get_json()
+    name = json_data['name']
+    resp = {'message': f'Hello, {name}'}
+    return resp, 200
+
---
+++ backend/main.py
@@ -0,0 +1,57 @@
+import json
+import os
+import vertexai
+from flask import Flask, request
+from vertexai.language_models import TextGenerationModel
+
+vertexai.init(location='asia-northeast1')
+generation_model = TextGenerationModel.from_pretrained('text-bison@002')
+app = Flask(__name__)
+
+
+def get_response(prompt, temperature=0.2):
+    response = generation_model.predict(
+        prompt, temperature=temperature, max_output_tokens=1024
+    )
+    return response.text.lstrip()
+
+
+@app.route('/api/correction', methods=['POST'])
+def grammar_correction():
+    json_data = request.get_json()
+    text = json_data['text']
+    # Join multiple lines into a single line
+    text = ''.join(text.splitlines())
+
+    prompt = '''\
+    「text:」以下の英文を正しい英文法に書き直してください。
+    書き直した文章のみを出力すること。
+    
+    text: 
+
+    text: {}
+    '''.format(text)
+    corrected = get_response(prompt)
+
+    prompt = '''\
+    「text:」以下の英文をより自然で洗練された英文に書き直した例を３つ示してください。
+    
+    text: I went to school yesterday. I ate an apple for lunch. I like eat apple.
+    answer:
+    - I went to school yesterday. I had an apple for lunch. I love apples.
+    - Yesterday, I went to school. I had an apple for lunch. I really enjoy eating apples.
+    - Yesterday, I went to school. I had an apple for lunch. Apples are my favorite fruit.
+    
+    
+    次が本当の質問です。これに回答してください。
+    text: {}
+    answer:
+    '''.format(text)
+    samples = get_response(prompt, temperature=0.4)
+
+    resp = {
+        'corrected': corrected,
+        'samples': samples
+    }
+
+    return resp, 200
